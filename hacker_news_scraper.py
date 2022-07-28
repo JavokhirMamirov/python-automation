75584d88c431380349cdc3dd6ba270bdf677cedc
Mamirov1997 <mamirovjavoxir@gmail.com>
2022-07-28 09:43:57 +0500

    hacker news scraper and send email

--- hacker_news_scraper.py
+++ hacker_news_scraper.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+import requests
+from bs4 import  BeautifulSoup
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+now = datetime.datetime.now()
 
+content = ''
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def extract_news(url):
+    print('Extracting Hacker News Stories ...')
+    cnt = ''
+    cnt += ('<b>HN Top Stories: </b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    for i, tag in enumerate(soup.find_all('td', attrs={'class':'title', 'valign':''})):
+        cnt += ((str(i+1)+' :: '+tag.text + "\n"+'<br>') if tag.text != 'More' else '')
+    return (cnt)
 
+cnt = extract_news('https://news.ycombinator.com/')
+content += cnt
+content += ('<br>------------<br>')
+content += ('<br><br>End of Message')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#send Email
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+SERVER = 'smtp.gmail.com'
+PORT = 587
+TO = 'example@gmail.com'
+FROM = 'example@gmail.com'
+PASS = '********'
+
+msg = MIMEMultipart()
+
+msg['Subject'] = 'Top News Stories HN [Automated Email]'+" "+str(now.day)+"-"+str(now.month)+'-'+str(now.year)
+
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+
+print('Initiating Server ...')
+server = smtplib.SMTP(SERVER, PORT)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email Sent ...')
+
+server.quit()
